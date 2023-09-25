c16f4007a22a1a5c3f80432edc8ce2120e218380
mikaelagillett <mikaelaihgillett@gmail.com>
2023-09-25 14:34:09 -0400

    added current drop down function

--- src/CurrentDropDown.js
+++ src/CurrentDropDown.js
@@ -1,28 +1,46 @@
+import { useState } from "react";
+
 function CurrentDropDown() {
-  return (
-    <div className="currentDropDown">
-      <button className="drop-down-button" id="drop-down-button">
-        <i className="fa-solid fa-circle-chevron-down"></i>
-      </button>
-      <div className="current-drop-down hidden" id="drop-down-section">
-        <span className="weather-description" id="weather-description"></span>
-        <ul className="descriptors">
-          <li className="feels-like">
-            feels like: <span id="current-feels-like"></span>
-          </li>
-          <li className="humidity">
-            humidity: <span id="current-humidity"></span>
-          </li>
-          <li className="wind-speed">
-            wind speed: <span id="current-wind-speed"></span>
-          </li>
-        </ul>
-        <button className="close-drop-down-button" id="close-drop-down-button">
-          <i className="fa-solid fa-circle-chevron-up"></i>
+  const [dropDown, setDropDown] = useState("closed");
+  function openDropDown() {
+    setDropDown("open");
+  }
+
+  function closeDropDown() {
+    setDropDown("closed");
+  }
+
+  if (dropDown === "closed") {
+    return (
+      <div className="currentDropDown">
+        <button className="drop-down-button" onClick={openDropDown}>
+          <i className="fa-solid fa-circle-chevron-down"></i>
         </button>
       </div>
-    </div>
-  );
+    );
+  } else if (dropDown === "open") {
+    return (
+      <div className="currentDropDown">
+        <div className="current-drop-down">
+          <span className="weather-description"></span>
+          <ul className="descriptors">
+            <li className="feels-like">
+              feels like: <span id="current-feels-like"></span>
+            </li>
+            <li className="humidity">
+              humidity: <span id="current-humidity"></span>
+            </li>
+            <li className="wind-speed">
+              wind speed: <span id="current-wind-speed"></span>
+            </li>
+          </ul>
+          <button className="close-drop-down-button" onClick={closeDropDown}>
+            <i className="fa-solid fa-circle-chevron-up"></i>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default CurrentDropDown;
