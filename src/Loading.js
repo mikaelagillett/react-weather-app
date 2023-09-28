827117fea7dc00e729639749f7e3dfaba4eedc9f
mikaelagillett <mikaelaihgillett@gmail.com>
2023-09-28 09:58:58 -0400

    added react loader spinner to loading page

--- src/Loading.js
+++ src/Loading.js
@@ -1,5 +1,20 @@
+import { Oval } from "react-loader-spinner";
+
 function Loading() {
-  return <h1>Loading</h1>;
+  return (
+    <Oval
+      height={80}
+      width={80}
+      color="#a5b9c0"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+      ariaLabel="oval-loading"
+      secondaryColor="#a5b9c0"
+      strokeWidth={2}
+      strokeWidthSecondary={2}
+    />
+  );
 }
 
 export default Loading;
